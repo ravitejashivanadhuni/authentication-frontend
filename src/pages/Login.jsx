085e5fab3b617be0abbc1392a4cdf43756b286e0
ravitejashivanadhuni <ravitejashivanadhuni@gmail.com>
2026-01-26 23:26:44 +0530

    made changes in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import { Sun, Moon, Eye, EyeOff, Github, Mail } from "lucide-react";
 import { useTheme } from "../context/ThemeContext";
 import { Link } from "react-router-dom";
 
-// Login Page with Theme Toggle and Social Logins
+// Login Page with Theme Toggle and Social Logins with Tailwind CSS
 export default function Login() {
   const navigate = useNavigate();
   const { isDarkMode, toggleTheme } = useTheme();
