cf998f6b9721745105e623b63d6a9e36f24f6620
ravitejashivanadhuni <ravitejashivanadhuni@gmail.com>
2026-01-27 23:10:44 +0530

    made changes in utils

--- src/utils/auth.jsx
+++ src/utils/auth.jsx
@@ -1,4 +1,4 @@
-//give me code for this utility checks auth status
+//this code over here checks auth status whether user is logged in or not
 export const isAuthenticated = () => {
   // Check for a token in localStorage (or cookies)
   const token = localStorage.getItem('token');
